7478a1098eb3bb5ea3787a95a8ec93b7ae62d2bc
Guido Sterbini <guido.sterbini@cern.ch>
2026-06-22 15:32:23 +0200

    Status

--- 000_dipolar_effect.py
+++ 000_dipolar_effect.py
@@ -6,11 +6,6 @@ import xtrack as xt
 collider = xt.Multiline.from_json('/home/kaltchev/collider_wires.json')
 lhcb1 = collider.lines['lhcb1']
 
-
-# %% Twiss 4D 
-
-lhcb1.twiss4d()
-
 # %% Inspection of the wires
 
 # Extract all wire element names in lhcb1
@@ -23,7 +18,7 @@ for wire in sorted(wire_elements):
 
 # %%
 
-bbcw_elements = ['bbcw.a.4l1.b1', 'bbcw.a.4l5.b1', 'bbcw.b.4l1.b1', 'bbcw.b.4l5.b1']
+bbcw_elements = ['bbcw.a.4l1.b1', 'bbcw.b.4l1.b1', 'bbcw.a.4l5.b1', 'bbcw.b.4l5.b1']
 
 # Get twiss data to extract s-positions
 twiss = lhcb1.twiss4d()
@@ -34,10 +29,13 @@ for elem_name in bbcw_elements:
     # Get s-position
     elem_index = lhcb1.element_names.index(elem_name)
     s_pos = twiss.s[elem_index]
-
+    x_co = twiss.x[elem_index]
+    y_co = twiss.y[elem_index]
     print("=" * 70)
     print(f"Element: {elem_name}")
-    print(f"S-position: {s_pos:.6f} m")
+    print(f"s-position: {s_pos:.6f} m")
+    print(f"x-coordinate co: {x_co:.6f} m")
+    print(f"y-coordinate co: {y_co:.6f} m")
     print("=" * 70)
     print(f"Type: {type(element).__name__}\n")
 
@@ -66,21 +64,41 @@ lhcb1.ref['i_wire.a.4l1.b1'].xdeps.info(limit=None)
 
 lhcb1.element_refs['bbcw.a.4l1.b1'].post_subtract_py.xdeps.info(limit=None)
 
-lhcb1.ref['ksl_0_wire.a.4l1.b1'].xdeps.info(limit=None)
+lhcb1.ref['knl_0_wire.a.4l1.b1'].xdeps.info(limit=None)
+
 # %%
+# d_wire.a.4l1.b1
+lhcb1['d_wire.a.4l1.b1']
+# %%
+# co_x_wire.a.4l1.b1
+lhcb1['co_x_wire.a.4l1.b1']
+
+# %%
+
+lhcb1['on_x1'] = 0
+
 lhcb1.ref['i_wire.a.4l1.b1'] = 0
+lhcb1.ref['i_wire.b.4l1.b1'] = 0
 
 
-for current in ['i_wire.a.4l1.b1','i_wire.a.4l5.b1','i_wire.b.4l1.b1','i_wire.b.4l5.b1']:
-    print(f"Current {current}: {lhcb1.ref[current].xdeps.value} A") 
-    
-print(80 *"=")
+def print_wire_currents(lhcb1, wire_names):
+    print(80 *"=")
+    for current in wire_names:
+        print(f"Current {current}: {lhcb1.ref[current].xdeps.value} A") 
+    print(80 *"=")
+   
+print_wire_currents(lhcb1, ['i_wire.a.4l1.b1','i_wire.b.4l1.b1','i_wire.a.4l5.b1','i_wire.b.4l5.b1'])
 
 tw_zero_current  = lhcb1.twiss4d()
 
-lhcb1.ref['i_wire.a.4l1.b1'] = 350
-for current in ['i_wire.a.4l1.b1','i_wire.a.4l5.b1','i_wire.b.4l1.b1','i_wire.b.4l5.b1']:
-    print(f"Current {current}: {lhcb1.ref[current].xdeps.value} A") 
+lhcb1.ref['i_wire.a.4l1.b1'] = 100
+lhcb1.ref['i_wire.b.4l1.b1'] = 100
+
+print(lhcb1.element_refs['bbcw.a.4l1.b1'].post_subtract_py.xdeps.value)
+print(lhcb1.element_refs['bbcw.b.4l1.b1'].post_subtract_py.xdeps.value)
+
+print_wire_currents(lhcb1, ['i_wire.a.4l1.b1','i_wire.b.4l1.b1','i_wire.a.4l5.b1','i_wire.b.4l5.b1'])
+
 
 tw_100_current  = lhcb1.twiss4d()
 
@@ -88,8 +106,8 @@ tw_100_current  = lhcb1.twiss4d()
 # %%
 from matplotlib import pyplot as plt
 plt.figure(figsize=(10, 6))
-plt.plot(tw_zero_current.s,  tw_zero_current.x- tw_100_current.x, label='x (0 A)', color='blue')
-plt.plot(tw_zero_current.s, tw_zero_current.y- tw_100_current.y, label='y (0 A)', color='red')
+plt.plot(tw_zero_current.s, tw_zero_current.x - tw_100_current.x, label='DeltaX (0-100 A)', color='blue')
+plt.plot(tw_zero_current.s, tw_zero_current.y - tw_100_current.y, label='DeltaY (0-100 A)', color='red')
 plt.xlabel('s [m]')
 plt.ylabel('Delta [m]')
 plt.legend()
